12208e08137028697c36eeab49a0c01077b339e7
akash-tambake <140740430+akash-tambake@users.noreply.github.com>
2025-04-23 18:08:14 +0530

    Still Updating

---
+++ IDP Temp/assets/js/main.js
@@ -0,0 +1,79 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/* Menu Show */
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu Hidden*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== ADD BLUR HEADER ===============*/
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+
+
+/*=============== SWIPER PLANETS ===============*/ 
+
+const swiperTravel = new Swiper('.travel__swiper', {
+    loop:true,
+    spaceBetween: '32',
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+
+    breakpoints:{
+        600:{
+            slidesPerView:2,
+        },
+        900:{
+            slidesPerView:3,
+        },
+    }
+ 
+  });
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
