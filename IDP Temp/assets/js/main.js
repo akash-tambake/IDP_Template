66e048c67d6b7a940bd6e9fb011642e2eded6510
akash-tambake <140740430+akash-tambake@users.noreply.github.com>
2025-05-05 22:46:44 +0530

    Completed

--- IDP Temp/assets/js/main.js
+++ IDP Temp/assets/js/main.js
@@ -70,10 +70,64 @@ const swiperTravel = new Swiper('.travel__swiper', {
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '80px',
+    duration: 2500,
+    delay: 300,
+    // reset: true,
+
+})
+
+sr.reveal(`.home__data, .travel__swiper, .contact__container`)
+sr.reveal(`.home_img`, {origin:'bottom'})
+sr.reveal(`.home__ovni`, {delay: 800})
+sr.reveal(`explore_img`, {origin:'left'})
+sr.reveal(`.explore__data`, {origin:'right'})
+sr.reveal(`.explore__planet`,{origin: 'right', delay: 800})
+sr.reveal(`.history__card`,{interval: 100})
+sr.reveal(`.history__planet-1`,{origin: 'left', delay: 1000})
+sr.reveal(`.history__planet-2`,{origin: 'right', delay: 1200})
+sr.reveal(`.footer__planet-1`,{origin: 'bottom', delay: 600})
+sr.reveal(`.footer__planet-2`,{delay: 800})
+
+
+
+
+ 
+
+
 
